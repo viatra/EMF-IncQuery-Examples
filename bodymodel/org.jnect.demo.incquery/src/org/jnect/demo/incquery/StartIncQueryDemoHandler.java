b3db36a1c7d442bc6bd161a46b7100831f11bba4
Istvan Rath <istvan.rath@gmail.com>
2012-09-20 15:53:44 +0200

    some updates to bodymodel example

--- bodymodel/org.jnect.demo.incquery/src/org/jnect/demo/incquery/StartIncQueryDemoHandler.java
+++ bodymodel/org.jnect.demo.incquery/src/org/jnect/demo/incquery/StartIncQueryDemoHandler.java
@@ -7,7 +7,7 @@ import org.eclipse.viatra2.emf.incquery.runtime.exception.IncQueryException;
 import org.eclipse.viatra2.gtasm.patternmatcher.incremental.rete.misc.DeltaMonitor;
 import org.jnect.core.KinectManager;
 
-import bodymodel.jump.JumpMatcher;
+//import bodymodel.jump.JumpMatcher;
 import bodymodel.righthandabovehead.RightHandAboveHeadMatch;
 import bodymodel.righthandabovehead.RightHandAboveHeadMatcher;
 
