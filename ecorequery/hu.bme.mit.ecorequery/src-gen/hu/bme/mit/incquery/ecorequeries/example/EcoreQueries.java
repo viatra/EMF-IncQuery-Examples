80b340b53b76ba1df099f0d5e0757694b8bdb2ec
bergmann <bergmann@mit.bme.hu>
2013-05-10 17:09:48 +0200

    [405291] committing some missing generated artifacts

---
+++ ecorequery/hu.bme.mit.ecorequery/src-gen/hu/bme/mit/incquery/ecorequeries/example/EcoreQueries.java
@@ -0,0 +1,44 @@
+package hu.bme.mit.incquery.ecorequeries.example;
+
+import hu.bme.mit.incquery.ecorequeries.example.EClassAttributeMatcher;
+import hu.bme.mit.incquery.ecorequeries.example.EClassMatcher;
+import hu.bme.mit.incquery.ecorequeries.example.EClassNameMatcher;
+import hu.bme.mit.incquery.ecorequeries.example.EClassWithEStringAttributeMatcher;
+import hu.bme.mit.incquery.ecorequeries.example.ECoreNamedElementMatcher;
+import hu.bme.mit.incquery.ecorequeries.example.ECoreNamedElementNameMatcher;
+import hu.bme.mit.incquery.ecorequeries.example.EObjectMatcher;
+import hu.bme.mit.incquery.ecorequeries.example.EReferenceWithOneMultiplicityMatcher;
+import hu.bme.mit.incquery.ecorequeries.example.EReferenceWithOneMultiplicityNameMatcher;
+import hu.bme.mit.incquery.ecorequeries.example.EReferenceWithStarMultiplicityMatcher;
+import hu.bme.mit.incquery.ecorequeries.example.EReferenceWithStarMultiplicityNameMatcher;
+import hu.bme.mit.incquery.ecorequeries.example.IsEStringMatcher;
+import hu.bme.mit.incquery.ecorequeries.example.IsInECoreMatcher;
+import hu.bme.mit.incquery.ecorequeries.example.SampleQuery2Matcher;
+import hu.bme.mit.incquery.ecorequeries.example.SampleQueryMatcher;
+import hu.bme.mit.incquery.ecorequeries.example.SuperTypeOfMatcher;
+import hu.bme.mit.incquery.ecorequeries.example.SuperTypeOfNameMatcher;
+import org.eclipse.incquery.runtime.api.impl.BaseGeneratedPatternGroup;
+import org.eclipse.incquery.runtime.exception.IncQueryException;
+
+public final class EcoreQueries extends BaseGeneratedPatternGroup {
+  public EcoreQueries() throws IncQueryException {
+    querySpecifications.add(EReferenceWithOneMultiplicityMatcher.querySpecification());
+    querySpecifications.add(SampleQuery2Matcher.querySpecification());
+    querySpecifications.add(EReferenceWithStarMultiplicityNameMatcher.querySpecification());
+    querySpecifications.add(EClassAttributeMatcher.querySpecification());
+    querySpecifications.add(EClassWithEStringAttributeMatcher.querySpecification());
+    querySpecifications.add(EReferenceWithOneMultiplicityNameMatcher.querySpecification());
+    querySpecifications.add(IsInECoreMatcher.querySpecification());
+    querySpecifications.add(EObjectMatcher.querySpecification());
+    querySpecifications.add(SuperTypeOfNameMatcher.querySpecification());
+    querySpecifications.add(IsEStringMatcher.querySpecification());
+    querySpecifications.add(ECoreNamedElementNameMatcher.querySpecification());
+    querySpecifications.add(EClassMatcher.querySpecification());
+    querySpecifications.add(EClassNameMatcher.querySpecification());
+    querySpecifications.add(EReferenceWithStarMultiplicityMatcher.querySpecification());
+    querySpecifications.add(ECoreNamedElementMatcher.querySpecification());
+    querySpecifications.add(SuperTypeOfMatcher.querySpecification());
+    querySpecifications.add(SampleQueryMatcher.querySpecification());
+    
+  }
+}
