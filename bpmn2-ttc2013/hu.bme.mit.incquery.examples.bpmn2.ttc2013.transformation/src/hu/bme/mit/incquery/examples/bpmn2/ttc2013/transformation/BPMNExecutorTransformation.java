c5b8f9c3c162aa9ada37160af424f4b686f6ea0b
bergmann <bergmann@mit.bme.hu>
2013-06-19 17:20:59 +0200

    moved conflict set resolution

--- bpmn2-ttc2013/hu.bme.mit.incquery.examples.bpmn2.ttc2013.transformation/src/hu/bme/mit/incquery/examples/bpmn2/ttc2013/transformation/BPMNExecutorTransformation.java
+++ bpmn2-ttc2013/hu.bme.mit.incquery.examples.bpmn2.ttc2013.transformation/src/hu/bme/mit/incquery/examples/bpmn2/ttc2013/transformation/BPMNExecutorTransformation.java
@@ -1,10 +1,40 @@
 package hu.bme.mit.incquery.examples.bpmn2.ttc2013.transformation;
 
+import java.util.Set;
+
 import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.incquery.examples.bpmn.ttc2013.simulator.FairRandomConflictResolver;
+import org.eclipse.incquery.runtime.api.IncQueryEngine;
+import org.eclipse.incquery.runtime.evm.api.EventDrivenVM;
+import org.eclipse.incquery.runtime.evm.api.ExecutionSchema;
+import org.eclipse.incquery.runtime.evm.api.RuleSpecification;
+import org.eclipse.incquery.runtime.evm.specific.Schedulers;
+import org.eclipse.incquery.runtime.evm.specific.event.IncQueryEventRealm;
+import org.eclipse.incquery.runtime.evm.specific.scheduler.UpdateCompleteBasedScheduler.UpdateCompleteBasedSchedulerFactory;
+import org.eclipse.incquery.runtime.exception.IncQueryException;
 
 public class BPMNExecutorTransformation {
 
-    public void execute(ResourceSet set) {
+    public void execute(ResourceSet model) {
+    	// TODO runtime modell összerakása
+    	try {
+			IncQueryEngine engine = IncQueryEngine.on(model);
+			UpdateCompleteBasedSchedulerFactory scheduling = Schedulers.getIQEngineSchedulerFactory(engine);
+			Set<RuleSpecification<?>> rules = null; // TODO add rules
+			ExecutionSchema execSchema = EventDrivenVM.createExecutionSchema(IncQueryEventRealm.create(engine), scheduling);
+			execSchema.setConflictResolver(new FairRandomConflictResolver());
+			for (RuleSpecification<?> rule : rules) {
+				execSchema.addRule(rule);
+			}
+			execSchema.
+			execSchema.dispose();
+			
+			
+		} catch (IncQueryException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    	
 
     }
 }
