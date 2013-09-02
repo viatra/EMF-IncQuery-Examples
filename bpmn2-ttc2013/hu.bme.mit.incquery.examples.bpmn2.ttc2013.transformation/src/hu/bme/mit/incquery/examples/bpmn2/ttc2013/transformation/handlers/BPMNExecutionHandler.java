3133540577607736353a862e5b9d4a07742cbb87
Istvan David <goerenh@gmail.com>
2013-09-03 00:15:34 +0200

    refactored TTC2013 code
    up to task #3

--- bpmn2-ttc2013/hu.bme.mit.incquery.examples.bpmn2.ttc2013.transformation/src/hu/bme/mit/incquery/examples/bpmn2/ttc2013/transformation/handlers/BPMNExecutionHandler.java
+++ bpmn2-ttc2013/hu.bme.mit.incquery.examples.bpmn2.ttc2013.transformation/src/hu/bme/mit/incquery/examples/bpmn2/ttc2013/transformation/handlers/BPMNExecutionHandler.java
@@ -3,6 +3,8 @@ package hu.bme.mit.incquery.examples.bpmn2.ttc2013.transformation.handlers;
 import hu.bme.mit.incquery.examples.bpmn2.ttc2013.transformation.BPMNExecutorTransformation;
 import hu.bme.mit.incquery.runtime.bpmn2.BPMNModelConnector;
 
+import java.io.IOException;
+
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
@@ -23,18 +25,28 @@ public class BPMNExecutionHandler extends AbstractHandler implements IHandler {
     public Object execute(ExecutionEvent event) throws ExecutionException {
         final BPMNModelConnector modelConnector = new BPMNModelConnector(HandlerUtil.getActiveEditor(event));
         final ResourceSet set = (ResourceSet) modelConnector.getNotifier(IModelConnectorTypeEnum.RESOURCESET);
+        
+//        Bpmn2DiagramEditorInput input = (Bpmn2DiagramEditorInput)HandlerUtil.getActiveEditor(event).getEditorInput();
+//        String modelUri = input.getModelUri().toString();
+
         final IFile file = (IFile) HandlerUtil.getActiveEditor(event).getEditorInput().getAdapter(IFile.class);
         final String modelUri = file.getFullPath().toString();
-        final Resource modelResource = set.getResource(URI.createPlatformResourceURI(modelUri, false), false);
-        TransactionalEditingDomain editingDomain = TransactionUtil.getEditingDomain(modelResource);
+        
+		final Resource modelResource = set.getResource(URI.createPlatformResourceURI(modelUri, false), true);
+        TransactionalEditingDomain editingDomain = TransactionUtil.getEditingDomain(set);
         Resource executionResource = 
         		editingDomain.createResource(
-        				URI.createPlatformResourceURI(modelUri.concat(".bpmn20exec"), false).toString());
-        
+        				URI.createURI(modelUri.concat(".bpmn20exec"), false).toString());
         
         final BPMNExecutorTransformation transformation = new BPMNExecutorTransformation();
         transformation.execute(editingDomain, modelResource, executionResource);
-
+        
+        try {
+			executionResource.save(null);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
         return null;
     }
 
